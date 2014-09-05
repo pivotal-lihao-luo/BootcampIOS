d07404142016232775cb0443b1e95d32f950f94a
DEV FLOATER 33 <devfloater33@piv-wifi-19-165.pivotallabs.com>
2014-09-05 16:52:40 -0400

    Initial Commit

---
+++ Bootcamp/BCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCAppDelegate.h
+//  Bootcamp
+//
+//  Created by DEV FLOATER 33 on 9/5/14.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bootcamp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bootcamp
+//
+//  Created by DEV FLOATER 33 on 9/5/14.
+//  Copyright (c) 2014 Pivotal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCAppDelegate class]));
+    }
+}
